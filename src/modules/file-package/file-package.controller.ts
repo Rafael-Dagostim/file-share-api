ddb7edcfbed4dec551f1c414273b7103a1091b7e
Rafael da Silva Dagostim <rafaeldagostim.pessoal@gmail.com>
2024-02-27 19:46:53 -0300

    feat(file-package): add `findOne` and `create` routes

--- src/modules/file-package/file-package.controller.ts
+++ src/modules/file-package/file-package.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { FilePackageEntity } from './entities/file-package.entity';
 import { FilePackageService } from './file-package.service';
+import { FilePackageCreateDto } from './dto/file-package-create.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('file-package')
+@ApiTags('File Package')
 export class FilePackageController {
   public constructor(private readonly filePackageService: FilePackageService) {}
 
   @Get()
   public findAll(): Promise<FilePackageEntity[]> {
     return this.filePackageService.findAll();
+  }
+
+  @Get(':id')
+  public findOne(@Param('id') id: string): Promise<FilePackageEntity> {
+    return this.filePackageService.findOne(id);
+  }
+
+  @Post()
+  public create(@Body() dto: FilePackageCreateDto): Promise<FilePackageEntity> {
+    return this.filePackageService.create(dto);
   }
 }
--- src/modules/file-package/file-package.service.ts
+++ src/modules/file-package/file-package.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
 import { FilePackageEntity } from './entities/file-package.entity';
+import { FilePackageCreateDto } from './dto/file-package-create.dto';
 
 @Injectable()
 export class FilePackageService {
   public constructor(private readonly prismaService: PrismaService) {}
 
   public async findAll(): Promise<FilePackageEntity[]> {
-    const packages = await this.prismaService.filePackage.findMany();
-    return packages.map((pack) => new FilePackageEntity(pack));
+    const filePackages = await this.prismaService.filePackage.findMany();
+    return filePackages.map((pack) => new FilePackageEntity(pack));
+  }
+
+  public async findOne(id: string): Promise<FilePackageEntity> {
+    const filePackage = await this.prismaService.filePackage.findUniqueOrThrow({ where: { id } });
+    return new FilePackageEntity(filePackage);
+  }
+
+  public async create(dto: FilePackageCreateDto): Promise<FilePackageEntity> {
+    const filePackage = await this.prismaService.filePackage.create({ data: dto });
+    return new FilePackageEntity(filePackage);
   }
 }
